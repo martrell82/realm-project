e67f446acb11e6a2f58a7a5160ea91fbdd852681
Martrell Leonard <creative.trell@gmail.com>
2023-06-10 18:25:18 -0400

    all contents add

---
+++ js/data.js
@@ -0,0 +1,446 @@
+let civilizations = [
+
+    {
+        name: 'Pira',
+        planet: 'Piraka',
+        population: '430,830,103',
+        communication: 'Whistling like dialect',
+        transportation: 'Bubble like vehicle',
+        biology: {
+            height: '14 to 18 feet',
+            weight: '220 to 400 pounds',
+            movement: 'Walking motion',
+            defense: 'Vocal sound wave'
+        }
+    },
+    {
+        name: 'Levee',
+        planet: 'Levanon',
+        population: '3,532,000,000',
+        communication: 'Humming like dialect',
+        transportation: 'Square metal vehicles',
+        biology: {
+            height: '1 to 4 feet',
+            weight: '20 to 100 pounds',
+            movement: 'Sliding motion',
+            defense: 'Inteligence'
+        }
+    },
+    {
+        name: 'Boldir',
+        planet: 'Boldar',
+        population: '1,030,780',
+        communication: 'Grunting sound',
+        transportation: 'Wooden like tank',
+        biology: {
+            height: '20 to 30 feet',
+            weight: '5,000 to 10,000 pounds',
+            movement: 'Walking like motion',
+            defense: 'Strength'
+        }
+    },
+    {
+        name: 'Heru',
+        planet: 'Nelo 1',
+        population: '245,290,020',
+        communication: 'Telepathy',
+        transportation: 'Self floating',
+        biology: {
+            height: '3 to 7 feet',
+            weight: '20 to 230 pounds',
+            movement: 'Floating',
+            defense: 'Vocal sound wave'
+        }
+    },
+    {
+        name: 'Nu',
+        planet: 'Nugon',
+        population: '50,238,030,500',
+        communication: 'Emotion like communication',
+        transportation: 'Rolling steel vehicles',
+        biology: {
+            height: '1 to 5 feet',
+            weight: '10 to 140 pounds',
+            movement: 'Tip toe walking motion',
+            defense: 'Numbers'
+        }
+    },
+    {
+        name: 'Aguarians',
+        planet: 'Agua',
+        population: '2,530,500',
+        communication: 'Sonar like communication',
+        transportation: 'Aqua like jet pack',
+        biology: {
+            height: '4 to 8 feet',
+            weight: '30 to 250 pounds',
+            movement: 'Swimming like motion',
+            defense: 'Sharp bone like hand blades'
+        }
+    },
+    {
+        name: 'Haroc',
+        planet: 'Oxon',
+        population: '2,450,779,111',
+        communication: 'Human like communication',
+        transportation: 'Air ship',
+        biology: {
+            height: '4 to 7 feet',
+            weight: '50 to 300 pounds',
+            movement: 'Bird like flight',
+            defense: 'Metal spears'
+        }
+    },
+    {
+        name: 'Cepians',
+        planet: 'Sypher',
+        population: '500,829,110',
+        communication: 'Mental Telepathy',
+        transportation: 'Flying vehicles',
+        biology: {
+            height: '3 to 6.5 feet',
+            weight: '20 to 200 pounds',
+            movement: 'Walking like motion',
+            defense: 'Hand to Hand combat'
+        }
+    },
+    {
+        name: 'Graguc',
+        planet: 'Emrite',
+        population: '750,322,005',
+        communication: 'Mumbling like sound',
+        transportation: 'Disk shaped air craft',
+        biology: {
+            height: '5 to 15 feet',
+            weight: '60 to 450 pounds',
+            movement: 'Walking like motion',
+            defense: 'Lazer like gun'
+        }
+    },
+    {
+        name: 'Meno',
+        planet: 'Maropolus',
+        population: '65,114,820',
+        communication: 'High pitched verbal communication',
+        transportation: 'Floating air pod',
+        biology: {
+            height: '12 inches to 2.5 feet',
+            weight: '1 to 25 pounds',
+            movement: 'Walking like motion',
+            defense: 'Telekinesis'
+        }
+    },
+    {
+        name: 'Palians',
+        planet: 'Nector',
+        population: '4,270,114,820',
+        communication: 'Vibrating like vocals',
+        transportation: 'Magnetic like vehicle',
+        biology: {
+            height: '7 to 11 feet',
+            weight: '220 to 700 pounds',
+            movement: 'Hovering',
+            defense: 'Psycological minipulation'
+        }
+    },
+    {
+        name: 'Elba',
+        planet: 'Utika',
+        population: '63,888,091,660',
+        communication: 'Sign language like communication',
+        transportation: 'Vibration like scooter',
+        biology: {
+            height: '2 to 5 feet',
+            weight: '20 to 120 pounds',
+            movement: 'Hopping like walking motion',
+            defense: 'Speed'
+        }
+    },
+    {
+        name: 'Clabuni',
+        planet: 'Clabis',
+        population: '70,290,119',
+        communication: 'Tone like communication',
+        transportation: 'Unknown Kangaroo like animal',
+        biology: {
+            height: '8 to 16 feet',
+            weight: '300 to 2,080 pounds',
+            movement: 'Walking like motion',
+            defense: 'Arrow like weapon'
+        }
+    },
+    {
+        name: 'Solem',
+        planet: 'Legrom',
+        population: '7,653,067,330',
+        communication: 'Human like communication',
+        transportation: 'Advanced Gravity type vehicle',
+        biology: {
+            height: '6 to 18 feet',
+            weight: '100 to 1,300 pounds',
+            movement: 'Walking like motion',
+            defense: 'Creativity'
+        }
+    },
+    {
+        name: 'Woo',
+        planet: 'Mers',
+        population: '18,999,327',
+        communication: 'Owl like communication',
+        transportation: 'Airplane like suite',
+        biology: {
+            height: '5 to 9 feet',
+            weight: '90 to 390 pounds',
+            movement: 'Bird like walking motion',
+            defense: 'Boomerang like disc'
+        }
+    },
+    {
+        name: 'Mecto',
+        planet: 'Mectom 2',
+        population: '22,001,225,042',
+        communication: 'Electric like vocals',
+        transportation: 'Teleporting device',
+        biology: {
+            height: '6 to 7 feet',
+            weight: '140 to 250 pounds',
+            movement: 'Robotic like walking motion',
+            defense: 'Lazer like beam'
+        }
+    },
+    {
+        name: 'Xilorians',
+        planet: 'Bomet',
+        population: '4,270,114,820',
+        communication: 'Vibrating like vocals',
+        transportation: 'Magnetic like vehicle',
+        biology: {
+            height: '7 to 11 feet',
+            weight: '220 to 700 pounds',
+            movement: 'Levitation like motion',
+            defense: 'Strong vibrational sound wave'
+        }
+    },
+    {
+        name: 'Piots',
+        planet: 'Piotri',
+        population: '350,428,550',
+        communication: 'Hissing like communication',
+        transportation: 'Oxygen powered plant like vehicle',
+        biology: {
+            height: '16 to 22 feet',
+            weight: '190 to 350 pounds',
+            movement: 'Sliding like motion',
+            defense: 'Poisinous fume'
+        }
+    },
+    {
+        name: 'Leo',
+        planet: 'Gi',
+        population: '3,270,114,820',
+        communication: 'Echo like communication',
+        transportation: 'Floating suites',
+        biology: {
+            height: '3 to 6 feet',
+            weight: '10 to 250 pounds',
+            movement: 'Space like hopping',
+            defense: 'High powered light device'
+        }
+    },
+    {
+        name: 'Earthlings 2',
+        planet: 'New Earth',
+        population: '25,389,110,221',
+        communication: 'Vocal like communication',
+        transportation: 'Flying electric motor vehicle',
+        biology: {
+            height: '2 to 8 feet',
+            weight: '20 to 220 pounds',
+            movement: 'Walking like motion',
+            defense: 'Electric like gun'
+        }
+    },
+    {
+        name: 'Fikins',
+        planet: 'Nazar',
+        population: '10,883,452,993',
+        communication: 'Whispering like communication',
+        transportation: 'Lighting like air craft',
+        biology: {
+            height: '14 to 19 feet',
+            weight: '300 to 550 pounds',
+            movement: 'Walking like motion',
+            defense: 'Venom like substance'
+        }
+    },
+    {
+        name: 'Oran',
+        planet: 'Selenis',
+        population: '2,737,329,332',
+        communication: 'Echo like vocals',
+        transportation: 'Time travel like device',
+        biology: {
+            height: '3 to 7 feet',
+            weight: '280 to 360 pounds',
+            movement: 'Crawling like motion',
+            defense: 'Venomous like spider web'
+        }
+    },
+    {
+        name: 'Coppi',
+        planet: 'Boket Ecos',
+        population: '1,189,944,067',
+        communication: 'Oxygen consumption like sounds',
+        transportation: 'Unknown',
+        biology: {
+            height: '30 to 70 feet',
+            weight: '1,000 to 4,000 pounds',
+            movement: 'Crawling like motion',
+            defense: 'Physical consumption'
+        }
+    },
+    {
+        name: 'Trazta',
+        planet: 'Traztaria',
+        population: '89,699,288',
+        communication: 'Clicking like sound communication',
+        transportation: 'Rolling wheel like vehicle',
+        biology: {
+            height: '8 to 12 feet',
+            weight: '400 to 780 pounds',
+            movement: 'Skating like motion',
+            defense: 'Exploding metal balls'
+        }
+    },
+    {
+        name: 'Iti',
+        planet: 'Siredona',
+        population: '9,883,452,993',
+        communication: 'Static like vocal communication',
+        transportation: 'Unkown portal like technology',
+        biology: {
+            height: '3 to 5 feet',
+            weight: '30 to 150 pounds',
+            movement: 'Walking like motion',
+            defense: 'Electric shock ability'
+        }
+    },
+    {
+        name: 'Satopians',
+        planet: 'Satopia',
+        population: '35,156,972,005',
+        communication: 'Human like vocals',
+        transportation: 'Floating thin metal plates',
+        biology: {
+            height: '6 to 13 feet',
+            weight: '110 to 560 pounds',
+            movement: 'Movement by hand',
+            defense: 'Mental frequency wave'
+        }
+    },
+    {
+        name: 'Wokomas',
+        planet: 'Illustus',
+        population: '758,877,211',
+        communication: 'Human like vocals',
+        transportation: 'Wagon like vehicle',
+        biology: {
+            height: '7 to 12 feet',
+            weight: '380 to 1,700 pounds',
+            movement: 'Walking like motion',
+            defense: 'Intense eye laser beam'
+        }
+    },
+    {
+        name: 'Proto',
+        planet: 'Protomus',
+        population: '120,754,003,253',
+        communication: 'Telepathy',
+        transportation: 'Unknown teleporting device',
+        biology: {
+            height: '2 to 7 feet',
+            weight: '20 to 280 pounds',
+            movement: 'Hovering like motion',
+            defense: 'Microwave like force field'
+        }
+    },
+    {
+        name: 'Wizidos',
+        planet: 'Wizid 9',
+        population: '4,046,833,555',
+        communication: 'Human like vocals',
+        transportation: 'none',
+        biology: {
+            height: '3 to 6 feet',
+            weight: '20 to 190 pounds',
+            movement: 'Levitation',
+            defense: 'Reality distortion'
+        }
+    },
+    {
+        name: 'Vieght',
+        planet: 'Vipinus',
+        population: '743,999,664',
+        communication: 'Feline like communication',
+        transportation: 'Vacuum like vehical',
+        biology: {
+            height: '1 to 4 feet',
+            weight: '5 to 60 pounds',
+            movement: 'Lower feline mobility',
+            defense: '10 to 12 inch claws'
+        }
+    },
+    {
+        name: 'Actus',
+        planet: 'Jalinius',
+        population: '20,423,044,546',
+        communication: 'Antenna like communication',
+        transportation: 'Unknown electric vehicle',
+        biology: {
+            height: '10 to 17 feet',
+            weight: '500 to 3,000 pounds',
+            movement: 'Walking like motion',
+            defense: 'Electric mind weapon'
+        }
+    },
+    {
+        name: 'Unu',
+        planet: 'Goclocus',
+        population: '56,632,222,105',
+        communication: 'Bird like communication',
+        transportation: 'None',
+        biology: {
+            height: '80 to 90 feet',
+            weight: 'Unknown',
+            movement: 'Bird like flight',
+            defense: 'Vocal Echo'
+        }
+    },
+    {
+        name: 'Lilac',
+        planet: 'Latus 88',
+        population: '126,732,293,618',
+        communication: 'Facial expressions and hand movements',
+        transportation: 'Highly intellelligent monorail system',
+        biology: {
+            height: '1 to 3 feet',
+            weight: 'Unknown',
+            movement: 'Walking like motion',
+            defense: 'Superior ability to hide'
+        }
+    },
+    {
+        name: 'Konola',
+        planet: 'Strodin M',
+        population: '199,223,388',
+        communication: 'Mumbling like sound',
+        transportation: 'Unknown',
+        biology: {
+            height: '1,000 to 3,000 feet',
+            weight: 'Unknown',
+            movement: 'Walking like motion',
+            defense: 'Extreme durability'
+        }
+    }
+    
+]
